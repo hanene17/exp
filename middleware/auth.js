7b99c222c4b41cbd419a0b4077c08bce891e468b
amyLee <ghribihanen17@gmail.com>
2021-07-18 18:17:45 +0100

    commentF

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,17 +3,26 @@ const moment = require('moment');
 const startDay= moment.weekdays(2);
 const endDay= moment.weekdays(6);
 const startHour= moment('09:00:00', "hh:mm:ss")
-const endtHour= moment('10:00:00', "hh:mm:ss")
+const endtHour= moment('17:00:00', "hh:mm:ss")
 const isAuth= function(req, res, next){
 let durationday= moment().isBetween(startDay, endDay);
 let durationHour= moment().isBetween(startHour, endtHour);
-if(durationday || durationHour) {
-   
-    res.sendFile(path.join(__dirname,"/views/page401.pug"));
-console.log(__dirname, "/views/page.pug") 
 
+if(durationday || durationHour) {
+    next()
 }
- //res.status(401).send("you are not authorized come-back... soon!")
+    //res.status(401).sendFile(path.join(__dirname +"/views/page401.pug"));
+    //, {title: "Sorry, page not found"}
+//    else {
+//        res.sendFile(__dirname + "/views/page401.pug");
+//    console.log(__dirname+ "/views/page.pug") }
+else {
+    console.log("Sorry, page not found");
+    res.status(401).sendFile(__dirname+ '/page401.html', {title: "Sorry, page not found"})
+  
+}
+      
+         //res.status(401).send("you are not authorized come-back... soon!")
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -7,35 +7,45 @@ let home=  require('./home');
 let service= require('./service')
 let contact= require('./contact')
 //let page401 = require('./page401')
-const isAuth= require('../middleware/auth')
-
-/* GET home page. */
-router.get('/', home.get_home);
- router.get('/service', service.get_service);
- router.get('/contact', contact.get_contact);
-
- //app.use(app.router);
- app.use('/', router);
- app.use('/service', router);
- app.use('/contact', router);
-
+var isAuth= require('../middleware/auth')
 app.use(express.json()); //bodyparser
 app.use(isAuth);
+app.use(express.static(__dirname + '/public'));
+var currenttime = function (req, res, next) {
+  req.currenttime = Date.now();
+  next();
+};
+app.use(currenttime);
+
+app.use(express.static(path.join(__dirname+ 'views')));
+/* GET home page. */
+app.get('/', home.get_home);
+app.get('/service', service.get_service);
+app.get('/contact', contact.get_contact);
+
+
+
+ //app.use(app.router);
+//  app.use('/', router);
+//  app.use('/service', router);
+//  app.use('/contact', router);
+
+
 //https://www.linkedin.com/pulse/how-node-js-middleware-process-ayush-neekhra
 
     // https://expressjs.com/fr/4x/api.html#res.sendFile
     app.use(express.json());
-    app.use(express.urlencoded({ extended: false }));
+    //app.use(express.urlencoded({ extended: false }));
     //app.use(cookieParser());
-    app.use(express.static(path.join(__dirname, 'public')));
-    app.set('views', path.join(__dirname, 'views'));
-    app.set('view engine', 'pug');
-    app.get('*', function(req, res){
-        res.status(401).render('page401.pug', {title: "Sorry, page not found"});
-      });
+    //app.use(express.static(path.join(__dirname, '/public')));
+    // app.set('views', path.join(__dirname, 'views'));
+    // app.set('view engine', 'pug');
+    //  app.get('*', function(req, res, next){
+    //   next(res.status(401).render('page401.pug', {title: "Sorry, page not found"}))   ;
+    //   });
    
    //router.use(logger());
   
  
 
- module.exports = router;
+ module.exports = app;
